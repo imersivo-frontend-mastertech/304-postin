83b9a430b50fa9e365c7e88364310e70a4851777
Bruno Pimentel <brunoapimentel@gmail.com>
2018-02-08 22:43:48 -0200

    Finaliza lógica de registro de atividades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  entrada = '';
+
+  atividades = [];
+
+  constructor(){
+    let dadosEmString = localStorage.getItem('dados');
+
+    if(dadosEmString){
+      this.atividades = JSON.parse(dadosEmString);
+    }
+    
+  }
+
+  inserir(){
+    if(this.entrada != ''){
+      this.atividades.push(this.entrada);
+      this.entrada = '';
+    }
+
+    let dadosEmString = JSON.stringify(this.atividades);
+
+    localStorage.setItem('dados', dadosEmString);
+    
+    console.log(this.atividades);
+  }
+
+  limpar(){
+    this.atividades = [];
+    localStorage.removeItem('dados');
+  }
 }
--- src/app/atividade/atividade.component.ts
+++ src/app/atividade/atividade.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-atividade',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./atividade.component.scss']
 })
 export class AtividadeComponent implements OnInit {
+  @Input() texto;
 
   constructor() { }
 
